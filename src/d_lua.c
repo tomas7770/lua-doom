ef6fddbf1f97a5026941f4c1979141582b063577
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2024-01-24 12:05:30 -0100

    Fix losing context between Lua script lines

--- src/d_lua.c
+++ src/d_lua.c
@@ -8,15 +8,19 @@
 #include "i_printf.h"
 #include "w_wad.h"
 #include "memio.h"
+#include "i_system.h" // I_Realloc
 
-#define LUA_BUFFERMAX 1024 // input buffer area size, hardcoded for now
+#define LUA_BUFFER_START_SIZE 1024
 
 void ProcessLuaLump(int lumpnum)
 {
     MEMFILE* lump;
-    char inbuffer[LUA_BUFFERMAX];
+    int n;
     int error;
     lua_State *L;
+    char* inbuffer = (char*) malloc(sizeof(char)*LUA_BUFFER_START_SIZE);
+    ssize_t lua_buffer_size = sizeof(char)*LUA_BUFFER_START_SIZE;
+    int offset = 0;
 
     {
         void *buf = W_CacheLumpNum(lumpnum, PU_STATIC);
@@ -28,17 +32,23 @@ void ProcessLuaLump(int lumpnum)
     luaL_openlibs(L);
 
     // loop until end of file
-    while (mem_fgets(inbuffer, sizeof(inbuffer), lump))
+    while ((n = mem_fread(inbuffer+offset, sizeof(char), LUA_BUFFER_START_SIZE, lump)))
     {
-        error = luaL_loadbuffer(L, inbuffer, strlen(inbuffer), "line") ||
-                lua_pcall(L, 0, 0, 0);
-        if (error) {
-            fprintf(stderr, "%s", lua_tostring(L, -1));
-            lua_pop(L, 1);  /* pop error message from the stack */
+        offset += sizeof(char)*n;
+        if (offset >= lua_buffer_size) {
+            lua_buffer_size *= 2;
+            inbuffer = I_Realloc(inbuffer, lua_buffer_size);
         }
+    }
+    inbuffer[offset] = '\0';
+    error = luaL_loadbuffer(L, inbuffer, strlen(inbuffer), "LUAHACK") ||
+            lua_pcall(L, 0, 0, 0);
+    if (error) {
+        fprintf(stderr, "%s", lua_tostring(L, -1));
+        lua_pop(L, 1);  /* pop error message from the stack */
     }
 
     lua_close(L);
-
     mem_fclose(lump);
+    free(inbuffer);
 }
